85ba0df8af27c68073cf33e8451f1fae6cab9249
Igor Souza <igormcsouza@gmail.com>
2020-10-22 22:53:42 -0300

    fixing tests for wiki search

--- whatabot/tests/test_whatabot.py
+++ whatabot/tests/test_whatabot.py
@@ -11,3 +11,13 @@ def test_subject_query():
     assert query_result != None, 'Nothing has returned as result'
     assert type(query_result) == str, 'The returning is not a String'
     assert 'dog' in query_result, 'The subject may be not related to the query'
+
+def test_subject_query_fails():
+    query_result = wikipedia.query_by_subject('averyridiculousstring')
+
+    assert query_result != None, 'Nothing has returned as result'
+    assert type(query_result) == str, 'The returning is not a String'
+    assert 'averyridiculousstring' in query_result, 'The subject may be not \
+        related to the query'
+    assert 'Not sure what is' in query_result, 'It did not returned what was \
+        expected'
