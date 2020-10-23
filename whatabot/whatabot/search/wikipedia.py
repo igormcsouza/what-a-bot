e8bd4dc5c12dedd248e52acba0acfe5b42a758cd
Igor Souza <igormcsouza@gmail.com>
2020-10-22 22:42:02 -0300

    fixing error handling on wiki search

--- whatabot/whatabot/search/wikipedia.py
+++ whatabot/whatabot/search/wikipedia.py
@@ -6,8 +6,7 @@ wiki_wiki = wikipediaapi.Wikipedia('en')
 def query_by_subject(subject: str) -> str:
     page = wiki_wiki.page(subject)
 
-    assert page.exists() == True
-
-    print(page.summary)
+    if page.exists() != True:
+        return f'Not sure what is {subject}'
 
     return page.summary
